d8179503877d370fd0619e97dc1c05784eea2f45
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-08-22 03:57:15 +0000

    Add files via upload

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -8,5 +8,7 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         None
     """
+    if my_list is None:
+        return
     for idx in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[idx]))
