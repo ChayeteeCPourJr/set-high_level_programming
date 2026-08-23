c4340467f0793c1a088164d76091aad26a8eeeca
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-08-23 22:05:59 +0000

    Add files via upload

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a MyList class that inherits from list."""
+"""Defines a MyList class that inherits from list"""
 
 
 class MyList(list):
-    """A list with a method to print its sorted contents."""
+    """Represents a list of integers, with a sorted-print method"""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
+        """Print the list, sorted in ascending order"""
         print(sorted(self))
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""Defines the BaseGeometry class."""
+"""Defines a BaseGeometry class"""
 
 
 class BaseGeometry:
-    """Base class for geometry."""
+    """Represents a base for other geometry classes"""
 
     def area(self):
-        """Raise an exception because area is not implemented."""
-        raise Exception("area() is not implemented")
+        """Raise an exception; area computation must be implemented
+        by a subclass
+
+        Raises:
+            NotImplementedError: always
+        """
+        raise NotImplementedError("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
+        """Validate that value is a positive integer
+
+        Args:
+            name: the name of the attribute being validated
+            value: the value to validate
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
