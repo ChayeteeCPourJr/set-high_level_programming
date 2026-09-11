cb0975fb25141d23714ea42e70bc6d5aa361efe8
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-09-11 07:54:53 +0000

    Add files via upload

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print("{:c}".format(122 - i), end="")
-    else:
-        print("{:c}".format(122 - i - 32), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
